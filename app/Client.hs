cd651401f450355265f1c91acceda5a874f48ba7
Felix Zimmer <fzimmer@tuta.io>
2024-04-25 15:47:11 +0200

    Apply formatting

--- app/Client.hs
+++ app/Client.hs
@@ -1,24 +1,27 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE GADTs #-}
+
 module Client where
-import Graphics.Image (index, dims, readImageRGBA, VU (..))
-import Network.Socket
-    ( connect,
-      openSocket,
-      getAddrInfo,
-      defaultHints,
-      close,
-      withSocketsDo,
-      ServiceName,
-      HostName,
-      SocketType(Stream),
-      AddrInfo(addrAddress, addrSocketType),
-      Socket )
+
 import qualified Control.Exception as E
-import Network.Socket.ByteString (sendAll)
-import Graphics.Image.Interface (toWord8, ColorSpace (toComponents))
 import qualified Data.ByteString.Char8 as BS
 import Data.Word (Word8)
+import Graphics.Image (VU (..), dims, index, readImageRGBA)
+import Graphics.Image.Interface (ColorSpace (toComponents), toWord8)
+import Network.Socket
+  ( AddrInfo (addrAddress, addrSocketType),
+    HostName,
+    ServiceName,
+    Socket,
+    SocketType (Stream),
+    close,
+    connect,
+    defaultHints,
+    getAddrInfo,
+    openSocket,
+    withSocketsDo,
+  )
+import Network.Socket.ByteString (sendAll)
 import Text.Printf (printf)
 
 portNumber :: String
@@ -36,27 +39,25 @@ imageLoop :: Socket -> IO ()
 imageLoop sock = do
   img <- readImageRGBA VU "die.png"
   let (xBound, yBound) = dims img
-  let idx = [(x,y) | x <- [0..xBound - 1], y <- [0..yBound - 1]]
+  let idx = [(x, y) | x <- [0 .. xBound - 1], y <- [0 .. yBound - 1]]
   let messages = map (generateMessage img) idx
   mapM_ (sendAll sock) messages
-  
-runTCPClient :: HostName -> ServiceName -> (Socket -> IO a) -> IO a
+
+runTCPClient :: HostName -> ServiceName -> (Socket -> IO ()) -> IO ()
 runTCPClient host port client = withSocketsDo $ do
-    addr <- resolve
-    E.bracket (open addr) close client
+  addr <- resolve
+  E.bracket (open addr) close client
   where
     resolve = do
-        let hints = defaultHints { addrSocketType = Stream }
-        head <$> getAddrInfo (Just hints) (Just host) (Just port)
+      let hints = defaultHints {addrSocketType = Stream}
+      head <$> getAddrInfo (Just hints) (Just host) (Just port)
     open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
-        connect sock $ addrAddress addr
-        return sock
-
+      connect sock $ addrAddress addr
+      return sock
 
 generateMessage img pos = BS.pack $ printf "Draw %s 0x%02x%02x%02x%02x\n" (show $ swap pos) r g b a
-  where 
+  where
     comps :: (Word8, Word8, Word8, Word8)
     comps@(r, g, b, a) = toComponents $ toWord8 <$> index img pos
 
     swap (x, y) = (y, x)
-
