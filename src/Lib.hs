050499d6d59bb71c56ab87545e8d59469ca1cf58
Felix Zimmer <fzimmer@tuta.io>
2024-04-29 14:17:52 +0200

    Working attoparsec

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
   ( handleCommand,
     Canvas,
@@ -6,6 +8,10 @@ module Lib
   )
 where
 
+import Control.Applicative (Alternative ((<|>)))
+import Control.Monad (void)
+import Data.Attoparsec.ByteString.Char8 (Parser, char, decimal, hexadecimal, parseOnly, skipSpace, string)
+import Data.ByteString.Char8 (pack)
 import Data.Tuple (swap)
 import Data.Vector.Storable.Mutable (IOVector, write)
 import Data.Word (Word32)
@@ -18,6 +24,7 @@ type Coord = (Int, Int)
 type Canvas = IOVector Word32
 
 data Command = Draw Coord Word32 | Help
+  deriving (Show, Eq)
 
 helpStr :: String
 helpStr = "To draw a Pixel, send a String of the Format: 'Draw (pos_x,pos_y) 0xRRGGBBAA\\n'\n"
@@ -39,6 +46,41 @@ parseCommand s
     w = words s
 parseCommand _ = Nothing
 
+commandParser :: Parser Command
+commandParser = helpParser <|> drawParser
+
+helpParser :: Parser Command
+helpParser = string "Help" >> pure Help
+
+drawParser :: Parser Command
+drawParser = do
+  void $ string "Draw"
+  skipSpace
+  coord <- coordParser
+  skipSpace
+  color <- hexParser
+  return $ Draw coord color
+
+coordParser :: Parser Coord
+coordParser = do
+  void $ char '('
+  skipSpace
+  x <- decimal
+  skipSpace
+  void $ char ','
+  skipSpace
+  y <- decimal
+  skipSpace
+  void $ char ')'
+  if isInBounds (x, y) then return (y, x) else fail "Coordinates are out of bounds"
+  where
+    isInBounds (x, y) = x < canvasWidth && x >= 0 && y < canvasHeight && y >= 0
+
+hexParser :: Parser Word32
+hexParser = do
+  void $ string "0x"
+  hexadecimal
+
 getValidPos :: String -> Maybe Coord
 getValidPos s = do
   coord <- (readMaybe s :: Maybe Coord)
@@ -53,6 +95,6 @@ runCommand (Draw (x, y) rgba) canvas _ = do
 
 handleCommand :: Canvas -> Handle -> String -> IO ()
 handleCommand canvas handle s =
-  case parseCommand s of
-    Nothing -> putStrLn $ printf "Invalid Command! '%s'" s
-    Just cmd -> runCommand cmd canvas handle
+  case parseOnly commandParser $ pack s of
+    Left err -> putStrLn $ printf "Invalid command (%s): %s" s err
+    Right cmd -> runCommand cmd canvas handle
